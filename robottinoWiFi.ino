03d01a6fc20a21e64ab2d4360d3465fa9a6841a4
Prof-Palitto <94290557+Prof-Palitto@users.noreply.github.com>
2022-11-23 13:52:23 +0100

    Update robottinoWiFi.ino

--- robottinoWiFi.ino
+++ robottinoWiFi.ino
@@ -2,6 +2,60 @@
 
 #define rxPin 10
 #define txPin 8
+#define In1 2 //Pin D2 dell'Arduino e' collegato al Input 1 del ponte H
+#define In2 4 //Pin D4 dell'Arduino e' collegato al Input 2 del ponte H
+#define In3 6 //Pin D6 dell'Arduino e' collegato al Input 3 del ponte H
+#define In4 7 //Pin D7 dell'Arduino e' collegato al Input 4 del ponte H
+#define EnM1 5 //M1 motore di sinistra
+#define EnM2 9 //M2 motore destro
+#define trig 11
+#define echo 12
+
+void Msinistra(String direzione){
+  if(direzione == "avanti"){
+    digitalWrite(In1, HIGH); //M1 avanti
+    digitalWrite(In2, LOW);
+  }else if(direzione == "indietro") {
+    digitalWrite(In1, LOW);  //M1 indietro
+     digitalWrite(In2, HIGH);}
+   else {
+    digitalWrite(In1, LOW);
+    digitalWrite(In2, LOW);}
+}
+
+void Mdestra(String direzione){
+  if(direzione == "avanti"){
+    digitalWrite(In3, HIGH); //M2 avanti
+    digitalWrite(In4, LOW);
+  } else if(direzione == "indietro") {
+      digitalWrite(In3, LOW); //M2 indietro
+      digitalWrite(In4, HIGH);
+
+  }else{
+    digitalWrite(In3, LOW);
+    digitalWrite(In4, LOW);
+  }
+}
+
+void robot(String azione){
+  if(azione == "gira in senso orario"){
+   Msinistra("avanti");
+   Mdestra("indietro");
+  }else if (azione == "gira in senso antiorario"){
+   Msinistra("indietro");
+      Mdestra("avanti");
+  }else if (azione == "vai avanti"){
+   Msinistra("avanti");
+      Mdestra("avanti");
+  }else if (azione == "vai indietro"){
+   Msinistra("indietro");
+      Mdestra("indietro");
+  }else if (azione == "fermati"){
+   Msinistra("stop");
+      Mdestra("stop");
+  }
+}
+
 // Set up a new SoftwareSerial object
 SoftwareSerial mySerial =  SoftwareSerial(rxPin, txPin);
 
@@ -25,6 +79,20 @@ String esp01cmd(String cmd) {
 }
 
 void setup()  {
+  pinMode(2, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(9, OUTPUT);
+  digitalWrite(EnM1, HIGH);
+  digitalWrite(EnM2, HIGH);
+ 
+   pinMode(trig,OUTPUT);
+  digitalWrite(trig,LOW);
+  delayMicroseconds(2);
+  pinMode(echo,INPUT);
+
     // Define pin modes for TX and RX
     pinMode(rxPin, INPUT);
     pinMode(txPin, OUTPUT);
@@ -120,9 +188,15 @@ void loop() {
         switch (cmd){
         case 'a': 
             printlnWIFI("Avanti di " + String(durata), cellphoneIP);
+            robot("vai avanti");
+            delay(durata);
+            robot("fermati");
             break;
         case 'i': 
             printlnWIFI("Indietro di " + String(durata), cellphoneIP);
+            robot("vai indietro");
+            delay(durata);
+            robot("fermati");
             break;
         default:
             printlnWIFI("Comando non esistente: ", cellphoneIP);
